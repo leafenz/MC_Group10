775c3bf459b69e90815776d5cac2ed89997c7b34
Nutdaya Tipdubpai <100523093@alumnos.uc3m.es>
2025-10-31 13:23:39 +0100

    Remove main execution block from main.py

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-from myfunction import Filter_three_Dimension, init_sharedarray, tonumpyarray
-import numpy as np
-import multiprocessing as mp
 
-if __name__ == "__main__":
-    mp.freeze_support()  # Mandatory in Windows!
-
-    image = np.random.rand(10, 10, 3) # Creating an image in this case to try it out
-    kernel = np.ones((3, 3, 3)) / 27
-
-    f3d = Filter_three_Dimension(image, kernel)
-
-    print("🔧 Applying parallel 3D filter...")
-    result = f3d.parallel_shared_image(num_process=4)
-
-    print("✅ Filtering completed.")
-    print("Forma de salida:", result.shape)
-    print("Primeros valores de la primera fila:\n", result[0, :, 0])
 
